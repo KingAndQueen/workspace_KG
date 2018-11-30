9c30c8cf08933e0a9573ba1802042e6ce7fd802e
young <qichuanyang@hotmail.com>
2018-11-30 13:47:59 +0800

    fix

--- Analysis.py
+++ Analysis.py
@@ -7,13 +7,13 @@ import pdb
 import numpy as np
 
 def drew_output_pic(data_patch,case_name,save_path):
-    pdb.set_trace()
+    # pdb.set_trace()
     for i in range(data_patch.shape[0]):
         plt.figure(figsize=(160, 320))
         plt.imshow(data_patch[i])
         if not os.path.exists(save_path):
             os.makedirs(save_path)
-        plt.savefig(save_path+case_name+'.png')
+        plt.savefig(save_path+str(case_name)+'.png')
         plt.close('all')
 
 def drew_seq(times,data_seq,save_path):
