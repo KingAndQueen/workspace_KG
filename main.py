c1f0c16490c2f7e9cb80820eed27de6948b5ab42
young <qichuanyang@hotmail.com>
2018-12-06 15:53:48 +0800

    vgg_gray_without_noise

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ def main(_):
     print('data processed,vocab size:', vocab.vocab_size)
     train_data,valid_test_data=model_selection.train_test_split(batches_data,test_size=0.2,shuffle=False)
     valid_data,test_data=model_selection.train_test_split(valid_test_data,test_size=0.5,shuffle=False)
-    # pdb.set_trace()
+    pdb.set_trace()
     sess = tf.Session()
     test_ignore_len=len(times)-len(batches_data)*config.batch_size
     if test_ignore_len>0:
@@ -129,7 +129,7 @@ def main(_):
         print('Reload model from checkpoints.....')
         ckpt = tf.train.get_checkpoint_state(config.checkpoint_path)
         model.saver.restore(sess, ckpt.model_checkpoint_path)
-        test_model(sess, model, test_data, vocab,times_test)
+        test_model(sess, model,train_data[:9], vocab,times_test)
 
 if __name__ == "__main__":
     tf.app.run()
