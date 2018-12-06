28e6201a37d27f4a3b3a5e99d8c898ff06465cb5
young <qichuanyang@hotmail.com>
2018-12-06 13:36:38 +0800

    vgg_tune_gray

--- Analysis.py
+++ Analysis.py
@@ -1,11 +1,12 @@
-
 import matplotlib
+
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import os
 import time
 import pdb
 import numpy as np
+
 
 def run_tm(func):
     def inner(*N):
@@ -15,40 +16,47 @@ def run_tm(func):
         tm = end - start
         # print('time : %f' % tm)
         return res, tm
+
     return inner
 
-def drew_output_pic(data_patch,case_name,save_path):
+
+def drew_output_pic(data_patch, case_name, save_path, gray=False):
     # pdb.set_trace()
     # for i in range(data_patch.shape[0]):
-        plt.figure(figsize=(16, 32))
+    plt.figure(figsize=(16, 32))
+    if gray:
+        plt.imshow(data_patch, 'gray')
+    else:
         plt.imshow(data_patch)
-        if not os.path.exists(save_path):
-            os.makedirs(save_path)
-        plt.savefig(save_path+str(case_name)+'.png')
-        plt.close('all')
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    plt.savefig(save_path + str(case_name) + '.png')
+    plt.close('all')
 
-def drew_seq(times,data_seq_batch,save_path):
-    data_seq=[]
+
+def drew_seq(times, data_seq_batch, save_path,gray=False):
+    data_seq = []
     for pic_batch in data_seq_batch:
         for pic in pic_batch:
             data_seq.append(pic)
-    assert len(times)==len(data_seq)
-    for idx,pic in enumerate(data_seq):
-        name=times[idx]
-        drew_output_pic(pic,name,save_path)
+    assert len(times) == len(data_seq)
+    for idx, pic in enumerate(data_seq):
+        name = times[idx]
+        drew_output_pic(pic, name, save_path,gray)
 
-def write_sents(times,data_seq_batch,save_path,vocab):
+
+def write_sents(times, data_seq_batch, save_path, vocab):
     data_seq = []
     # pdb.set_trace()
     for txt_batch in data_seq_batch:
         for txt in txt_batch:
             data_seq.append(txt)
 
-    assert len(times)==len(data_seq)
-    f=open(save_path+'test_output.txt','w')
-    for idx,txt2 in enumerate(data_seq):
+    assert len(times) == len(data_seq)
+    f = open(save_path + 'test_output.txt', 'w')
+    for idx, txt2 in enumerate(data_seq):
         # for txt in txt2:
-            sent=[vocab.index_to_word(word)+' ' for word in txt2]
-            f.writelines(sent)
-            f.write('\n')
+        sent = [vocab.index_to_word(word) + ' ' for word in txt2]
+        f.writelines(sent)
+        f.write('\n')
     f.close()
