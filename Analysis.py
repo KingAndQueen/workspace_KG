21c17a3e831d46d5c40d3a6f3f9a80ab0421b8b0
young <qichuanyang@hotmail.com>
2018-12-14 13:16:42 +0800

    add squeeze in testing drew the pic

--- Analysis.py
+++ Analysis.py
@@ -17,21 +17,24 @@ def run_tm(func):
         return res, tm
     return inner
 
-def drew_output_pic(data_patch,case_name,save_path):
+def drew_output_pic(data_patch, case_name, save_path, gray=False):
     # pdb.set_trace()
     # for i in range(data_patch.shape[0]):
-        plt.figure(figsize=(16, 32))
+    plt.figure(figsize=(16, 32))
+    if gray:
+        plt.imshow(np.squeeze(data_patch), cmap='gray')
+    else:
         plt.imshow(data_patch)
-        if not os.path.exists(save_path):
-            os.makedirs(save_path)
-        plt.savefig(save_path+str(case_name)+'.png')
-        plt.close('all')
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    plt.savefig(save_path + str(case_name) + '.png')
+    plt.close('all')
 
 def drew_seq(times,data_seq_batch,save_path):
     data_seq=[]
     for pic_batch in data_seq_batch:
         for pic in pic_batch:
-            data_seq.append(np.squeeze(pic))
+            data_seq.append(pic)
     assert len(times)==len(data_seq)
     for idx,pic in enumerate(data_seq):
         name=times[idx]
