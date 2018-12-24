851abe73a43467de3f6368b3b7dc2d4d325f6398
young <qichuanyang@hotmail.com>
2018-12-24 11:23:32 +0800

    tune -- DCGAN only square with noise and compare loss

--- Model.py
+++ Model.py
@@ -329,8 +329,8 @@ class seq_pic2seq_pic():
         d_loss_real = tf.reduce_mean(
             sigmoid_cross_entropy_with_logits(predict_pic, tf.ones_like(h4)))
         d_loss_fake = tf.reduce_mean(
-            sigmoid_cross_entropy_with_logits(self._output_pic, tf.ones_like(self._output_pic)))
-        d_loss =tf.square(d_loss_real+d_loss_fake)
+            sigmoid_cross_entropy_with_logits(self._output_pic, tf.zeros_like(self._output_pic)))
+        d_loss =tf.square(d_loss_real - d_loss_fake)
 
         # pic_loss = tf.sqrt(tf.reduce_sum(tf.square(tf.subtract(self._output_pic, predict_pic), name='pic_loss')))
         # cross_loss = tf.reduce_mean(pic_loss, name='l2_mean_loss_pic')
