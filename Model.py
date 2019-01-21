eb585b9156c87dca207b2240bace8480703cb293
young <qichuanyang@hotmail.com>
2019-01-21 13:08:38 +0800

    transformer with cnn encoder without decnn

--- Model.py
+++ Model.py
@@ -11,6 +11,7 @@ import convolution
 # from VGG import build_vgg19
 from transformer import *
 
+
 class seq_pic2seq_pic():
     def __init__(self, config, vocab):
         self._vocab = vocab
@@ -20,7 +21,7 @@ class seq_pic2seq_pic():
 
         # self._opt = tf.train.GradientDescentOptimizer(learning_rate=self._learn_rate)
         # self._opt = tf.train.AdamOptimizer(learning_rate=self._learn_rate)
-        self._opt= tf.train.AdamOptimizer(learning_rate=self._learn_rate, beta1=0.9, beta2=0.98, epsilon=1e-8)
+        self._opt = tf.train.AdamOptimizer(learning_rate=self._learn_rate, beta1=0.9, beta2=0.98, epsilon=1e-8)
 
         self._embedding_size = config.recurrent_dim
         self._layers = config.layers
@@ -31,53 +32,52 @@ class seq_pic2seq_pic():
         self._cov_size = config.convolution_dim
         self._noise_dim = config.noise_dim
         self.model_type = config.model_type
-        self._num_identical=config.num_identical
+        self._num_identical = config.num_identical
         self._build_inputs()
-        self._head =config.head
+        self._head = config.head
         self.g_bn0 = convolution.batch_norm(name='g_bn0')
         self.g_bn1 = convolution.batch_norm(name='g_bn1')
         self.g_bn2 = convolution.batch_norm(name='g_bn2')
         self.g_bn3 = convolution.batch_norm(name='g_bn3')
-        if self.model_type=='train':
-            is_training=True
+        if self.model_type == 'train':
+            is_training = True
         else:
-            is_training=False
+            is_training = False
 
         with tf.variable_scope("encode_txt"):
-                self.enc = embedding(self._question,
-                                     vocab_size=vocab.vocab_size,
-                                     num_units=self._embedding_size,
-                                     scale=True,
-                                     scope="enc_embed")
+            self.enc = embedding(self._question,
+                                 vocab_size=vocab.vocab_size,
+                                 num_units=self._embedding_size,
+                                 scale=True,
+                                 scope="enc_embed")
 
-                # Position Encoding(use range from 0 to len(inpt) to represent position dim of each words)
-                # tf.tile(tf.expand_dims(tf.range(tf.shape(self.inpt)[1]), 0), [tf.shape(self.inpt)[0], 1]),
-                self.enc += positional_encoding(self._question,
-                                                vocab_size=self._sentence_size,
-                                                num_units=self._embedding_size,
-                                                zero_pad=False,
-                                                scale=False,
-                                                scope="enc_pe")
+            # Position Encoding(use range from 0 to len(inpt) to represent position dim of each words)
+            # tf.tile(tf.expand_dims(tf.range(tf.shape(self.inpt)[1]), 0), [tf.shape(self.inpt)[0], 1]),
+            self.enc += positional_encoding(self._question,
+                                            vocab_size=self._sentence_size,
+                                            num_units=self._embedding_size,
+                                            zero_pad=False,
+                                            scale=False,
+                                            scope="enc_pe")
 
-                # Dropout
-                self.enc = tf.layers.dropout(self.enc,
-                                             rate=0.1,
-                                             training=tf.convert_to_tensor(is_training))
+            # Dropout
+            self.enc = tf.layers.dropout(self.enc,
+                                         rate=0.1,
+                                         training=tf.convert_to_tensor(is_training))
 
-                # Identical
-                for i in range(self._num_identical):
-                    with tf.variable_scope("num_identical_{}".format(i)):
-                        # Multi-head Attention
-                        self.enc = multihead_attention(queries=self.enc,
-                                                       keys=self.enc,
-                                                       num_units=self._embedding_size,
-                                                       num_heads=self._head,
-                                                       dropout_rate=0.1,
-                                                       is_training=is_training,
-                                                       causality=False)
+            # Identical
+            for i in range(self._num_identical):
+                with tf.variable_scope("num_identical_{}".format(i)):
+                    # Multi-head Attention
+                    self.enc = multihead_attention(queries=self.enc,
+                                                   keys=self.enc,
+                                                   num_units=self._embedding_size,
+                                                   num_heads=self._head,
+                                                   dropout_rate=0.1,
+                                                   is_training=is_training,
+                                                   causality=False)
 
-                        self.enc = feedforward(self.enc, num_units=[4 * self._embedding_size, self._embedding_size])
-
+                    self.enc = feedforward(self.enc, num_units=[4 * self._embedding_size, self._embedding_size])
 
         # pdb.set_trace()
         def _encoding_pic_frame(frame, name='', GPU_id=0):
@@ -85,36 +85,49 @@ class seq_pic2seq_pic():
                 # resident net
                 resnet_output, end_points = convolution.resnet_v2_50(frame)
                 # resnet_output=end_points['resnet_v2_50' + '/block4'] # test2 to check the full connections effect
-                encode_output=end_points['encoding_frame_'+name+'/resnet_v2_50/block4']
+                # encode_output=end_points['encoding_frame_'+name+'/resnet_v2_50/block4']
                 # pdb.set_trace()
-
-                return encode_output
+                w_pic = tf.get_variable('w', [1, 2048, 64], initializer=tf.random_normal_initializer(stddev=0.02))
+                resnet_output = tf.squeeze(resnet_output, 1)
+                encoding_pic_output = tf.nn.conv1d(resnet_output, w_pic, 1, 'SAME')
+                encoding_pic_output = tf.tile(encoding_pic_output, [1, 30, 1])
+                return encoding_pic_output
 
         # pdb.set_trace()
-        encoder_pic_output = _encoding_pic_frame(self._input_pic,'one')
+        encoder_pic_output = _encoding_pic_frame(self._input_pic, 'one')
 
-        with tf.variable_scope('decode_txt'):
-            self.decoder_input = tf.concat((tf.ones_like(self._response[:, :1]) * 2, self._response[:, :-1]), -1)
-            self.dec = embedding(self._response,
-                                 vocab_size=vocab.vocab_size,
-                                 num_units=self._embedding_size,
-                                 scale=True,
-                                 scope="dec_embed")
+        with tf.variable_scope('embed_decode_input'):
+
+            decoder_input = tf.concat((tf.ones_like(self._response[:, :1]) * 2, self._response[:, :-1]), -1)
+            dec = embedding(self._response,
+                            vocab_size=vocab.vocab_size,
+                            num_units=self._embedding_size,
+                            scale=True,
+                            scope="dec_embed")
 
             # Position Encoding(use range from 0 to len(inpt) to represent position dim)
-            self.dec += positional_encoding(self.decoder_input,
-                                            vocab_size=self._sentence_size,
-                                            num_units=self._embedding_size,
-                                            zero_pad=False,
-                                            scale=False,
-                                            scope="dec_pe")
+            dec += positional_encoding(decoder_input,
+                                       vocab_size=self._sentence_size,
+                                       num_units=self._embedding_size,
+                                       zero_pad=False,
+                                       scale=False,
+                                       scope="dec_pe")
 
+        with tf.variable_scope('merge_txt_pic'):
+            decoder_input = tf.concat((encoder_pic_output, dec), -1)
+            w_merge = tf.get_variable('w', [1, 128, 64], initializer=tf.random_normal_initializer(stddev=0.02))
+            # pdb.set_trace()
+            self.dec =tf.nn.conv1d(decoder_input, w_merge, 1, 'SAME')
+
+
+        with tf.variable_scope('decode_txt'):
             # Dropout
             self.dec = tf.layers.dropout(self.dec,
                                          rate=0.1,
                                          training=tf.convert_to_tensor(is_training))
-
             # Identical
+            # pdb.set_trace()
+
             for i in range(self._num_identical):
                 with tf.variable_scope("num_identical_{}".format(i)):
                     # Multi-head Attention(self-attention)
@@ -139,67 +152,67 @@ class seq_pic2seq_pic():
 
                     self.dec = feedforward(self.dec, num_units=[4 * self._embedding_size, self._embedding_size])
 
-        def deconv2d(input_, output_shape, k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02, name="deconv2d", with_w=False):
-            with tf.variable_scope(name):
-                # filter : [height, width, output_channels, in_channels]
-                w = tf.get_variable('w', [k_h, k_w, output_shape[-1], input_.get_shape()[-1]],
-                                    initializer=tf.random_normal_initializer(stddev=stddev))
+                    # def deconv2d(input_, output_shape, k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02, name="deconv2d", with_w=False):
+                    #     with tf.variable_scope(name):
+                    #         # filter : [height, width, output_channels, in_channels]
+                    #         w = tf.get_variable('w', [k_h, k_w, output_shape[-1], input_.get_shape()[-1]],
+                    #                             initializer=tf.random_normal_initializer(stddev=stddev))
+                    #
+                    #         deconv = tf.nn.conv2d_transpose(input_, w, output_shape=output_shape,
+                    #                                         strides=[1, d_h, d_w, 1])
+                    #
+                    #         biases = tf.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
+                    #         deconv = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
+                    #
+                    #         if with_w:
+                    #             return deconv, w, biases
+                    #         else:
+                    #             return deconv
 
-                deconv = tf.nn.conv2d_transpose(input_, w, output_shape=output_shape,
-                                                strides=[1, d_h, d_w, 1])
 
-                biases = tf.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
-                deconv = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
+                    # with tf.variable_scope('decoder_pic'):
+                    #     s = self.img_size_x
+                    #     y = self.img_size_y
+                    #     s2, s4, s8, s16 = int(s / 2), int(s / 4), int(s / 8), int(s / 16)
+                    #     y2, y4, y8, y16 = int(y / 2), int(y / 4), int(y / 8), int(y / 16)
+                    #     # encoder_pic_output_reshape = tf.reshape(encoder_pic_output, [self._batch_size, -1])
+                    #     # response_txt_reshape=tf.reshape(response_txt,[self._batch_size,-1])
+                    #     # encoder_txt_output_reshape=tf.reshape(encoder_txt_output,[self._batch_size,-1])
+                    #     # all_infor=tf.concat([response_txt_reshape,encoder_txt_output_reshape,encoder_pic_output_reshape],1)
+                    #     # try more input method to replace all_infor # test1
+                    #     # pdb.set_trace()
+                    #     # reduced_text_embedding = lrelu(linear(all_infor, self._cov_size, 'g_embedding'))
+                    #     # reduced_text_embedding = lrelu(all_infor)
+                    #     # z_concat = tf.concat([self._random_z, reduced_text_embedding],1)
+                    #     # z_ = linear(encoder_pic_output_reshape, self._cov_size * 8 * s16 * y16, 'g_h0_lin')
+                    #     z_=encoder_pic_output
+                    #     # pdb.set_trace()
+                    #     h0 = tf.reshape(z_, [-1, s16, y16, self._cov_size * 8])
+                    #     h0 = tf.nn.relu(self.g_bn0(h0, type=self.model_type))
+                    #
+                    #     h1 = deconv2d(h0, [self._batch_size, s8, y8, self._cov_size * 4], name='g_h1')
+                    #     h1 = tf.nn.relu(self.g_bn1(h1, type=self.model_type))
+                    #
+                    #     h2 = deconv2d(h1, [self._batch_size, s4, y4, self._cov_size * 2], name='g_h2')
+                    #     h2 = tf.nn.relu(self.g_bn2(h2, type=self.model_type))
+                    #
+                    #     h3 = deconv2d(h2, [self._batch_size, s2, y2, self._cov_size * 1], name='g_h3')
+                    #     h3 = tf.nn.relu(self.g_bn3(h3, type=self.model_type))
+                    #
+                    #     h4 = deconv2d(h3, [self._batch_size, s, y, 1], name='g_h4')
+                    #
+                    #     predict_pic = tf.tanh(h4) / 2. + 0.5
 
-                if with_w:
-                    return deconv, w, biases
-                else:
-                    return deconv
-
-
-        with tf.variable_scope('decoder_pic'):
-            s = self.img_size_x
-            y = self.img_size_y
-            s2, s4, s8, s16 = int(s / 2), int(s / 4), int(s / 8), int(s / 16)
-            y2, y4, y8, y16 = int(y / 2), int(y / 4), int(y / 8), int(y / 16)
-            # encoder_pic_output_reshape = tf.reshape(encoder_pic_output, [self._batch_size, -1])
-            # response_txt_reshape=tf.reshape(response_txt,[self._batch_size,-1])
-            # encoder_txt_output_reshape=tf.reshape(encoder_txt_output,[self._batch_size,-1])
-            # all_infor=tf.concat([response_txt_reshape,encoder_txt_output_reshape,encoder_pic_output_reshape],1)
-            # try more input method to replace all_infor # test1
-            # pdb.set_trace()
-            # reduced_text_embedding = lrelu(linear(all_infor, self._cov_size, 'g_embedding'))
-            # reduced_text_embedding = lrelu(all_infor)
-            # z_concat = tf.concat([self._random_z, reduced_text_embedding],1)
-            # z_ = linear(encoder_pic_output_reshape, self._cov_size * 8 * s16 * y16, 'g_h0_lin')
-            z_=encoder_pic_output
-            # pdb.set_trace()
-            h0 = tf.reshape(z_, [-1, s16, y16, self._cov_size * 8])
-            h0 = tf.nn.relu(self.g_bn0(h0, type=self.model_type))
-
-            h1 = deconv2d(h0, [self._batch_size, s8, y8, self._cov_size * 4], name='g_h1')
-            h1 = tf.nn.relu(self.g_bn1(h1, type=self.model_type))
-
-            h2 = deconv2d(h1, [self._batch_size, s4, y4, self._cov_size * 2], name='g_h2')
-            h2 = tf.nn.relu(self.g_bn2(h2, type=self.model_type))
-
-            h3 = deconv2d(h2, [self._batch_size, s2, y2, self._cov_size * 1], name='g_h3')
-            h3 = tf.nn.relu(self.g_bn3(h3, type=self.model_type))
-
-            h4 = deconv2d(h3, [self._batch_size, s, y, 1], name='g_h4')
-
-            predict_pic = tf.tanh(h4) / 2. + 0.5
-
-            # print '$$$$$$$$$$$$$$$$$$$$'
-            # print h4.get_shape().as_list()
-            # print '$$$$$$$$$$$$$$$$$$$$'
+                    # print '$$$$$$$$$$$$$$$$$$$$'
+                    # print h4.get_shape().as_list()
+                    # print '$$$$$$$$$$$$$$$$$$$$'
 
         # pdb.set_trace()
         def compute_error(real, fake):
             return tf.reduce_mean(tf.abs(fake - real))
             # diversity loss
 
-        with tf.variable_scope('loss_function_pic'):
+            # with tf.variable_scope('loss_function_pic'):
             # pdb.set_trace()
             # cov_input=convolution.deeplab_v3(predict_pic)
             # cov_output=convolution.deeplab_v3(self._output_pic)
@@ -220,9 +233,9 @@ class seq_pic2seq_pic():
             # G_loss = tf.reduce_sum(tf.reduce_min(content_loss)) * 0.999 + tf.reduce_sum(
             #     tf.reduce_mean(content_loss)) * 0.001
 
-            pic_loss=tf.sqrt(tf.reduce_sum(tf.square(tf.subtract(self._real_pic,predict_pic),name='pic_loss'),[1,2,3]))
-            pic_loss=tf.reduce_mean(pic_loss,name='l2_mean_loss_pic')
-            self.predict_pic = predict_pic
+            # pic_loss=tf.sqrt(tf.reduce_sum(tf.square(tf.subtract(self._real_pic,predict_pic),name='pic_loss'),[1,2,3]))
+            # pic_loss=tf.reduce_mean(pic_loss,name='l2_mean_loss_pic')
+            # self.predict_pic = predict_pic
 
         with tf.variable_scope('loss_function_txt'):
             # with tf.device('/device:GPU:1'):
@@ -241,7 +254,7 @@ class seq_pic2seq_pic():
             # txt_loss = tf.reduce_mean(cross_entropy_sentence, name="cross_entropy_sentences")
             self.logits = tf.layers.dense(self.dec, vocab.vocab_size)
             preds = tf.to_int32(tf.argmax(self.logits, axis=-1))
-            self.predict_txt=preds
+            self.predict_txt = preds
             self.istarget = tf.to_float(tf.not_equal(self._response, 0))
             self.acc = tf.reduce_sum(tf.to_float(tf.equal(preds, self._response)) * self.istarget) / (
                 tf.reduce_sum(self.istarget))
@@ -250,14 +263,14 @@ class seq_pic2seq_pic():
             # pdb.set_trace()
             txt_loss = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=self.y_smoothed)
             mean_txt_loss = tf.reduce_sum(txt_loss * self.istarget) / (tf.reduce_sum(self.istarget))
-        # all_loss = pic_loss + 0.0*txt_loss
-        # pdb.set_trace()
+            # all_loss = pic_loss + 0.0*txt_loss
+            # pdb.set_trace()
 
-        # all_loss=G_loss+txt_loss
-        # loss_=tf.concat([tf.expand_dims(G_loss,-1),tf.expand_dims(cross_entropy_sentence,-1)],1)
-        # all_loss=linear(loss_,1)
+            # all_loss=G_loss+txt_loss
+            # loss_=tf.concat([tf.expand_dims(G_loss,-1),tf.expand_dims(cross_entropy_sentence,-1)],1)
+            # all_loss=linear(loss_,1)
 
-            self.losses = mean_txt_loss+pic_loss
+            self.losses = mean_txt_loss #+ pic_loss
         # pdb.set_trace()
 
         # grads_and_vars_txt = self._opt.compute_gradients(mean_txt_loss)
@@ -302,8 +315,6 @@ class seq_pic2seq_pic():
 
             tf.summary.scalar('mean_loss', self.losses)
             self.merged = tf.summary.merge_all()
-
-
 
     def add_gradient_noise(self, t, stddev=1e-3, name=None):
         """
--- convolution.py
+++ convolution.py
@@ -469,7 +469,7 @@ def resnet_v2_50(inputs,
     blocks = [
         resnet_v2_block('block1', base_depth=64, num_units=3, stride=2),
         resnet_v2_block('block2', base_depth=128, num_units=4, stride=2),
-        resnet_v2_block('block3', base_depth=256, num_units=6, stride=1),
+        resnet_v2_block('block3', base_depth=256, num_units=6, stride=2), # 1=4, 10, 20, 2048
         resnet_v2_block('block4', base_depth=512, num_units=3, stride=1),
     ]
     # pdb.set_trace()
